716b818f5145ea0bc54990d8814f675494920bd3
Malgorzata Salawa <mksalawa@gmail.com>
2016-06-03 20:31:53 +0200

    physijs added, driving not working

--- src/ticket-to-ride.js
+++ src/ticket-to-ride.js
@@ -81,9 +81,23 @@ init(animate);
 
 function init(callback) {
 
+    Physijs.scripts.worker = SERVER_ADDRESS + '/lib/Physijs/physijs_worker.js';
+    Physijs.scripts.ammo = SERVER_ADDRESS + '/lib/ammo.js';
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xffffff);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', onWindowResize, false);
+
+    // camera and scene
+
     camera = new THREE.TargetCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
 
-    scene = new THREE.Scene();
+    scene = new Physijs.Scene();
+    scene.setGravity(new THREE.Vector3( 0, -50, 0 ));
+
     scene.fog = new THREE.Fog(0xffffff, 0, 750);
 
     var light = new THREE.DirectionalLight(0xffffff, 1.5);
@@ -99,78 +113,70 @@ function init(callback) {
 
     // floor
 
-    geometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
-    geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
-
-    for (var i = 0, l = geometry.vertices.length; i < l; i++) {
-
-        var vertex = geometry.vertices[i];
-        vertex.x += Math.random() * 20 - 10;
-        vertex.y += Math.random() * 2;
-        vertex.z += Math.random() * 20 - 10;
-
-    }
-
     var floorTexture = new THREE.ImageUtils.loadTexture(SERVER_ADDRESS + "images/grasslight-big.png");
-    floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-    floorTexture.repeat.set(10, 10);
-    var floorMaterial = new THREE.MeshBasicMaterial({map: floorTexture, side: THREE.DoubleSide});
-    var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.position.y = -0.5;
-    floor.rotation.x = Math.PI / 2;
+    var floorMaterial = Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({
+            map: floorTexture,
+            side: THREE.DoubleSide
+        }),
+        .8, // high friction
+        .4 // low restitution
+    );
+    floorMaterial.map.wrapS = floorMaterial.map.wrapT = THREE.RepeatWrapping;
+    floorMaterial.map.repeat.set(10, 10);
+
+    var floorGeometry = new THREE.BoxGeometry(1000, 1000, 10, 10, 10, 1);
+    // var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+
+    // floorGeometry.computeFaceNormals();
+    // floorGeometry.computeVertexNormals();
+
+    var floor = new Physijs.BoxMesh(
+        floorGeometry,
+        floorMaterial,
+        0 // mass
+    );
+    floor.rotation.x = -Math.PI / 2;
+    floor.position.y = -5;
+    floor.receiveShadow = true;
     scene.add(floor);
+
 
     // objects
 
     geometry = new THREE.BoxGeometry(20, 20, 20);
 
-    for (var i = 0, l = geometry.faces.length; i < l; i++) {
-
-        var face = geometry.faces[i];
-        face.vertexColors[0] = new THREE.Color().setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-        face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-        face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-
-    }
-
     for (var i = 0; i < 100; i++) {
-        material = new THREE.MeshPhongMaterial({
-            specular: 0xffffff,
-            shading: THREE.FlatShading,
-            vertexColors: THREE.VertexColors
-        });
+        var mat = Physijs.createMaterial(
+            new THREE.MeshLambertMaterial({
+                map: THREE.ImageUtils.loadTexture(SERVER_ADDRESS + "images/brick-wall.jpg")
+            }),
+            .8, // high friction
+            .4 // low restitution
+        );
+        var mesh = new Physijs.BoxMesh(geometry, mat);
 
-        var mesh = createMesh(geometry, "brick-wall.jpg");
         mesh.position.x = Math.floor(Math.random() * 20 - 10) * 20;
-        mesh.position.y = 5;
+        mesh.position.y = 50;
         mesh.position.z = Math.floor(Math.random() * 20 - 10) * 20;
+        mesh.__dirtyRotation = true;
         scene.add(mesh);
 
-        material.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
         objects.push(mesh);
     }
-    //
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(0xffffff);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    document.body.appendChild(renderer.domElement);
-
-    //
-
-    window.addEventListener('resize', onWindowResize, false);
-
 
     var carModelUrl = SERVER_ADDRESS + "models/panamera/panamera.js";
 
     // load ascii model
     var jsonLoader = new THREE.JSONLoader();
-    jsonLoader.load(carModelUrl, function (geometry, materials) {
+    jsonLoader.load(carModelUrl, function (car_geometry, car_materials) {
 
-        geometry.computeTangents();
-        car = THREE.SceneUtils.createMultiMaterialObject(geometry, materials);
+        car_geometry.computeTangents();
+
+        car = new Physijs.BoxMesh(
+            car_geometry,
+            new THREE.MeshFaceMaterial( car_materials )
+        );
         car.position.set(0, 4.5, -30);
         car.rotation.z = Math.PI;
         car.rotation.x = -Math.PI / 2;
@@ -203,12 +209,15 @@ function init(callback) {
 
 }
 
-function createMesh(geom, imageFile) {
-    var texture = THREE.ImageUtils.loadTexture(SERVER_ADDRESS + "images/" + imageFile);
-    var mat = new THREE.MeshPhongMaterial();
-    mat.map = texture;
-    var mesh = new THREE.Mesh(geom, mat);
-    return mesh;
+function createMesh(geom, texture) {
+    var mat = Physijs.createMaterial(
+        new THREE.MeshLambertMaterial({
+            map: texture
+        }),
+        .8, // high friction
+        .4 // low restitution
+    );
+    return new Physijs.BoxMesh(geom, mat);
 }
 
 function onWindowResize() {
@@ -250,6 +259,7 @@ function animate() {
     }
 
     camera.update();
+    scene.simulate();
     renderer.render(scene, camera);
     time = Date.now();
 }
